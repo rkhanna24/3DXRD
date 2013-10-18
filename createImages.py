0aca82bff5c7fd9c6e07345500c78c4999004dcc
Rohan Khanna <rk9455@gmail.com>
2013-10-18 17:50:01 -0500

    made algo to extract rings from binaries

--- createImages.py
+++ createImages.py
@@ -5,31 +5,49 @@ Created on Fri Oct 18 14:52:17 2013
 @author: tempuser
 """
 
+import numpy as np
 from toImage import combineFrames
 from toImage import toImage
 
 frame = 1
 frame_bytes = 2*2048**2
 header = 8192
-lower = 1795
+size = (2048,2048)
 
-filehead = '/media/Argonne Backup/FFfine/Ti7_PreHRM_PreLoad__005'
+directory = '/media/Argonne Backup/FFfine/'
+bg_name = directory + 'Ti7Test_00017.ge2'
+
+bg_file = open(bg_name,'rb')
+bg_file.seek(header)
+bg = bg_file.read(frame_bytes)
+bg_file.close()
+
+bg = np.fromstring(bg,dtype = 'uint16')
+bg.shape = size
+bg = np.double(bg)
+
+filehead = 'Ti7_PreHRM_PreLoad__005'
 im_num = 53
-filename = filehead + str(im_num)
-#filename = 'ring'
-#lower = 60
+filename = directory + filehead + str(im_num)
+
 f = open(filename,'rb')
-f.seek(header)
+head = f.read(header)
 
 im_data_hex = f.read(frame_bytes)
-im_data = combineFrames(im_data_hex, lower)
+im_data = combineFrames(im_data_hex, bg, size)
 i = 1
-while(len(im_data_hex) != 0):
+print("Combining Frames."),
+while(True):
+    if(i % 4 == 0):
+        print("."),
     im_data_hex = f.read(frame_bytes)
-    temp = combineFrames(im_data_hex, lower)
-    if(len(temp) != 0):
-        im_data = im_data + temp
+    if(len(im_data_hex) == 0):
+        break
+    temp = combineFrames(im_data_hex, bg, size)
+    im_data = np.maximum(im_data,temp)
     i = i+1
-
+print("")
+print("DONE")
 toImage(im_data)
+
 f.close()
--- toImage.py
+++ toImage.py
@@ -10,22 +10,26 @@ import matplotlib.pyplot as plt
 from scipy import stats
 
 def toImage(image_data):
-    image_data.shape = (2048,2048)
-    image_data = np.uint8(image_data)
-    plt.imshow(image_data)
+    image = np.clip(image_data,0,255)
+    image = np.uint8(image)
+    image = stats.threshold(image,threshmin=60,newval=0)
+    plt.imshow(image)
     plt.hot()
     plt.axis('off')
     plt.colorbar()
     plt.show()
     plt.savefig("image1.png")
 
-def combineFrames(im_data_hex, lower):
+def combineFrames(im_data_hex, bg, size):
     image_data = np.fromstring(im_data_hex,dtype='uint16')
-    image_data = stats.threshold(image_data,threshmin = lower, newval = 0)
+    image_data.shape = size;
+    image_data = np.double(image_data)
+    image_data = image_data - bg
+    image_data = np.clip(image_data,0, 2**16 - 1)
+    image_data = np.uint16(image_data)
     return image_data
     
 def findModes(im_data_hex):
     image_data = np.fromstring(im_data_hex,dtype = 'uint16')
     m = stats.mode(image_data)
     return m
-    
