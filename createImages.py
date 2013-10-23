601e1775078e1c401164c77d8982d0472841dd66
Rohan Khanna <rk9455@gmail.com>
2013-10-23 16:51:18 -0500

    Wrote algorithm to extract all rings, wrote algorithm to convert intensities to rgb vals

--- createImages.py
+++ createImages.py
@@ -2,14 +2,12 @@
 """
 Created on Fri Oct 18 14:52:17 2013
 
-@author: tempuser
+@author: Rohan Khanna
 """
 
 import numpy as np
-from toImage import combineFrames
 from toImage import toImage
 
-frame = 1
 frame_bytes = 2*2048**2
 header = 8192
 size = (2048,2048)
@@ -27,27 +25,50 @@ bg.shape = size
 bg = np.double(bg)
 
 filehead = 'Ti7_PreHRM_PreLoad__005'
-im_num = 53
-filename = directory + filehead + str(im_num)
 
-f = open(filename,'rb')
-head = f.read(header)
+def createImages(im_num,im_data):
+    
+    filename = directory + filehead + str(im_num)    
+    f = open(filename,'rb')
+    f.seek(header)    
+    i = 1
+    print("Combining Frames."),
+    while(True):
+        if(i % 4 == 0):
+            print("."),
+        im_data_hex = f.read(frame_bytes)
+        if(len(im_data_hex) == 0):
+            break
+        temp = combineFrames(im_data_hex, bg, size)
+        if(im_num == 53 and i == 1):
+            im_data = temp
+        else:
+            im_data = np.maximum(im_data,temp)
+        i = i+1       
+    print("")
+    print("DONE")    
+    f.close()
+    
+    return im_data
 
-im_data_hex = f.read(frame_bytes)
-im_data = combineFrames(im_data_hex, bg, size)
-i = 1
-print("Combining Frames."),
-while(True):
-    if(i % 4 == 0):
-        print("."),
-    im_data_hex = f.read(frame_bytes)
-    if(len(im_data_hex) == 0):
-        break
-    temp = combineFrames(im_data_hex, bg, size)
-    im_data = np.maximum(im_data,temp)
-    i = i+1
-print("")
-print("DONE")
-toImage(im_data)
+def combineFrames(im_data_hex, bg, size):
+    image_data = np.fromstring(im_data_hex,dtype='uint16')
+    image_data.shape = size;
+    image_data = np.double(image_data)
+    image_data = image_data - bg
+    image_data = np.clip(image_data,0, 2**16 - 1)
+    image_data = np.uint16(image_data)
+    return image_data
 
-f.close()
+ims = np.linspace(53,70,70-53+1)
+ims = np.uint8(ims)
+
+for n in ims:
+    if n == 53:
+        im = createImages(n,0)
+    else:
+        temp = createImages(n,im)
+        im = np.maximum(im,temp)
+
+im = np.maximum(im,0*im)
+toImage(im)
--- toImage.py
+++ toImage.py
@@ -8,28 +8,35 @@ Created on Thu Oct 10 18:22:43 2013
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
+import Image
 
+def toRGB(a, maxI):
+    if a <= 60:
+        return [0,0,0]
+    else:
+        white = 0xFFFFFF
+        slope = white/maxI
+        color = np.int(slope*a)
+        r = (color >> 8) & 0xFF
+        g = (color >> 4) & 0xFF
+        b = (color >> 0) & 0xFF
+        return [r,g,b]
+        
 def toImage(image_data):
-    image = np.clip(image_data,0,255)
-    image = np.uint8(image)
-    image = stats.threshold(image,threshmin=60,newval=0)
-    plt.imshow(image)
+    
+    plt.imshow(np.minimum(stats.threshold(image_data,threshmin=60,newval=0),255+0*image_data))
     plt.hot()
     plt.axis('off')
-    plt.colorbar()
     plt.show()
-    plt.savefig("image1.png")
-
-def combineFrames(im_data_hex, bg, size):
-    image_data = np.fromstring(im_data_hex,dtype='uint16')
-    image_data.shape = size;
-    image_data = np.double(image_data)
-    image_data = image_data - bg
-    image_data = np.clip(image_data,0, 2**16 - 1)
-    image_data = np.uint16(image_data)
-    return image_data
+    plt.savefig("ring1.png")
     
-def findModes(im_data_hex):
-    image_data = np.fromstring(im_data_hex,dtype = 'uint16')
-    m = stats.mode(image_data)
-    return m
+    maxI = np.max(image_data)
+    rgbArr = np.zeros((2048,2048,3),dtype = 'uint8')
+    
+    for i in range(2048):
+        for j in range(2048):
+            rgbArr[i,j] = toRGB(image_data[i][j],maxI)
+            
+    image = Image.fromarray(rgbArr,'RGB')
+    image.show()
+    image.save('ring2.png')
