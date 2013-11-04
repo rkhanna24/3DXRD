d27e126fcebcedda028c317b95e0134d6f2e78e4
Rohan Khanna <rk9455@gmail.com>
2013-11-04 13:51:50 -0600

    3d plot of data

--- radii.py
+++ radii.py
@@ -8,6 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 from readGE import convertBin
+from mpl_toolkits.mplot3d import Axes3D
 
 filename = 'ring'
 offset = 8192
@@ -17,32 +18,34 @@ f = open(filename,'rb')
 f.seek(offset)
 image_data = f.read()
 image_data = convertBin(image_data,np.zeros(size))
+x = np.arange(0,2048,1)
+y = np.arange(0,2048,1)
+X,Y = np.meshgrid(x,y)
 plt.ion()
-plt.imshow(np.minimum(stats.threshold(image_data,
-                        threshmin=60, newval=0), 255 + 0*image_data))
-
-plt.hot()
-
-plt.axis([0, 2048, 0, 2048])
-
-cx = 1024
-cy = 1024
-plt.plot([cx],[cx],'bo')
-thresh = 300
-for i in range(1024):
-    if(image_data[cx + i,cy + i] > thresh):
-        plt.plot([cx + i],[cy + i],'bo')
-    if(image_data[cx - i,cy - i] > thresh):
-        plt.plot([cx - i],[cy - i],'bo')
-    if(image_data[cx + i,cy - i] > thresh):
-        plt.plot([cx + i],[cy - i],'bo')
-    if(image_data[cx - i,cy + i] > thresh):
-        plt.plot([cx - i],[cy + i],'bo')
-    if(image_data[cx - i,cy] > thresh):
-        plt.plot([cx - i],[cy],'bo')
-    if(image_data[cx,cy - i] > thresh):
-        plt.plot([cx],[cy - i],'bo')
-    if(image_data[cx + i,cy] > thresh):
-        plt.plot([cx + i],[cy],'bo')
-    if(image_data[cx,cy + i] > thresh):
-        plt.plot([cx],[cy + i],'bo')
+fig = plt.figure()
+ax = fig.gca(projection = '3d')
+ax.scatter(X,Y,image_data)
+#plt.ion()
+#plt.imshow(np.minimum(stats.threshold(image_data,
+#                        threshmin=60, newval=0), 255 + 0*image_data))
+#
+#plt.hot()
+#
+#plt.axis([0, 2048, 0, 2048])
+#
+#cx = 1024
+#cy = 1024
+#plt.plot([cx],[cx],'bo')
+#thresh = 300
+#
+#t = np.linspace(0,2*np.pi,1000)
+#r1 = 410
+#r2 = 430
+#r3 = 440
+#plt.plot(r1*np.cos(t) + cx,r1*np.sin(t) + cy,'r-')
+#plt.plot(r2*np.cos(t) + cx,r2*np.sin(t) + cy,'r-')
+#plt.plot(r3*np.cos(t) + cx,r3*np.sin(t) + cy,'r-')
+#
+#for i in range(2048):
+#    for j in range (2048):
+#        if()
