5f73c81900b5ad377bcf9b7174ecee30130bd1cd
Rohan Khanna <rk9455@gmail.com>
2013-12-05 17:05:12 -0600

    checked correctness of maps

--- etaphi.py
+++ etaphi.py
@@ -6,56 +6,51 @@ Created on Thu Nov 21 15:09:59 2013
 """
 import numpy as np
 import matplotlib.pyplot as plt
+from readGE import convertBin
 
-import readGE
+def main():
+    params = 'circles.out'
+    
+    a = np.arange(0,2048)
+    yy = np.tile(a,[2048,1]) #y's
+    a.shape = [2048,1] 
+    xx = np.tile(a,[1,2048]) #x's
+    
+    #ring = np.loadtxt(params,delimiter = ',')
+    
+    #cenX = ring[0,0]
+    #cenY = ring[0,1]
+    cenX = cenY = 1024
+    
+    radius = np.sqrt((xx - cenX)**2 + (yy-cenY)**2)
+    ee = np.arctan2(cenY - yy, xx - cenX)*180/np.pi + 180
+    
+    plt.figure()
+    plt.imshow(radius)
+    plt.colorbar()
+    
+    plt.figure()
+    plt.imshow(ee)
+    plt.colorbar()
+    
+    #r = ring[0,2]
+    #d = 10
+    
+    r = 940
+    d = 20
+    
+    ii = (radius > (r - d)) & (radius < (r + d))
+    
+    eei = np.ceil(ee.T[ii.T]) # for compatability with MATLAB
+    eei[eei == 361] = 360
+    
+    plt.ion()
+    plt.figure()
+    plt.plot(np.arange(len(eei)),eei)
 
-folder = '/media/Argonne Backup/FFfine/'
-bgName = folder + 'Ti7Test_00017.ge2'
-fileHead = 'Ti7_PreHRM_PreLoad__005'
-params = 'circles.out'
-
-minID = 53 # ID of first binary
-maxID = 70 # ID of last binary
-
-pBar = False # Set to False if not in Python or iPython
-
-im = readGE.readGE( directory = folder, 
-                       filePrefix = fileHead, 
-                       bgFile = bgName,
-                       lowerID = minID, 
-                       upperID = maxID, 
-                       bar = pBar)
-
-a = np.arange(0,2048)
-yy = np.tile(a,[2048,1]) #y's
-a.shape = [2048,1] 
-xx = np.tile(a,[1,2048]) #x's
-
-ring = np.loadtxt(params,delimiter = ',')
-
-cenX = ring[0,0]
-cenY = ring[0,1]
-
-radius = np.sqrt((xx - cenX)**2 + (yy-cenY)**2)
-ee = np.arctan2(cenY - yy, xx - cenX)*180/np.pi + 180
-
-plt.figure()
-plt.imshow(radius)
-plt.colorbar()
-
-plt.figure()
-plt.imshow(ee)
-plt.colorbar()
-
-r = ring[0,2]
-d = 10
-
-ii = (radius > (r - d)) & (radius < (r + d))
-
-eei = np.ceil(ee.T[ii.T]) # for compatability with MATLAB
-
-plt.ion()
-plt.figure()
-plt.plot(np.arange(len(eei)),eei)
-
-etaInt = np.zeros((360,180))
+    etaInt = np.zeros((360,180))
+    
+def loadNext():
+    
+if __name__ == "__main__":
+    main()
