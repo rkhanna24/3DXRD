607a8a6d0b5f7b0c7cd32d944531ea53f6970ec0
Rohan Khanna <rk9455@gmail.com>
2013-12-05 18:19:03 -0600

    Made function that grabs one frame at a time and wrote code to integrate

--- etaphi.py
+++ etaphi.py
@@ -5,52 +5,54 @@ Created on Thu Nov 21 15:09:59 2013
 @author: tempuser
 """
 import numpy as np
-import matplotlib.pyplot as plt
-from readGE import convertBin
+#import matplotlib.pyplot as plt
+import getFrame
 
-def main():
-    params = 'circles.out'
-    
-    a = np.arange(0,2048)
-    yy = np.tile(a,[2048,1]) #y's
-    a.shape = [2048,1] 
-    xx = np.tile(a,[1,2048]) #x's
-    
-    #ring = np.loadtxt(params,delimiter = ',')
-    
-    #cenX = ring[0,0]
-    #cenY = ring[0,1]
-    cenX = cenY = 1024
-    
-    radius = np.sqrt((xx - cenX)**2 + (yy-cenY)**2)
-    ee = np.arctan2(cenY - yy, xx - cenX)*180/np.pi + 180
-    
-    plt.figure()
-    plt.imshow(radius)
-    plt.colorbar()
-    
-    plt.figure()
-    plt.imshow(ee)
-    plt.colorbar()
-    
-    #r = ring[0,2]
-    #d = 10
-    
-    r = 940
-    d = 20
-    
-    ii = (radius > (r - d)) & (radius < (r + d))
-    
-    eei = np.ceil(ee.T[ii.T]) # for compatability with MATLAB
-    eei[eei == 361] = 360
-    
-    plt.ion()
-    plt.figure()
-    plt.plot(np.arange(len(eei)),eei)
+params = 'circles.out'
+directory = '/media/Argonne Backup/FFfine/'
+bgFile = directory + 'Ti7Test_00017.ge2'
+filePrefix = 'Ti7_PreHRM_PreLoad__005'
 
-    etaInt = np.zeros((360,180))
-    
-def loadNext():
-    
-if __name__ == "__main__":
-    main()
+lowerID = 53 # ID of first binary
+upperID = 70 # ID of last binary
+
+
+a = np.arange(0,2048)
+yy = np.tile(a,[2048,1]) #y's
+a.shape = [2048,1] 
+xx = np.tile(a,[1,2048]) #x's
+
+ring = np.loadtxt(params,delimiter = ',')
+
+cenX = ring[0,0]
+cenY = ring[0,1]
+#cenX = cenY = 1024
+
+radius = np.sqrt((xx - cenX)**2 + (yy-cenY)**2)
+ee = np.arctan2(cenY - yy, xx - cenX)*180/np.pi + 180
+
+
+r = ring[0,2]
+d = 10
+
+#r = 940
+#d = 20
+
+ii = (radius > (r - d)) & (radius < (r + d))
+
+eei = np.ceil(ee.T[ii.T]) # for compatability with MATLAB
+eei[eei == 360] = 359
+
+etaInt = np.zeros((360,3600))
+IDs = np.linspace(lowerID, upperID, upperID - lowerID + 1)
+IDs = np.uint8(IDs)
+
+k = 0
+for ID in IDs:
+    for i in range(1,201):
+        im = getFrame.getFrame(directory,filePrefix, i, bgFile, ID)
+        imc = im.T[ii.T]
+        
+        for j in range(len(imc)):
+            etaInt[eei[j],k] = imc[j] + etaInt[eei[j],k]
+        k = k + 1
