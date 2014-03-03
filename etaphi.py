ff681389a3b4c5887cad7d0949d306a68c5e95ad
Rohan Khanna <rk9455@gmail.com>
2014-03-03 16:45:28 -0600

    Changed colormaps and testing embarassingly parallelization

--- etaphi.py
+++ etaphi.py
@@ -1,33 +1,87 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Nov 21 15:09:59 2013
+@File: etaphi.py
+@Author: Rohan Khanna
+@Date:Fri Nov 21, 2013
 
-@author: tempuser
+The following script converts the intensity data in all frames to eta-phi maps
+for each ring.
+This requires the rings parameters to be written to a file in the format:
+        x-coordinate of center, y-coordinate of center, radius
+This function is meant to be run in the python shell, with an argument 
+provided indicating the ring to be processed.
+    Ex: python etaphi.py 1
+
 """
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
-from readGE import convertBin
+import sys
+import time
+
+"""
+Edit the following parameters below to fit your tests
+"""
+
+params = 'circles.out' # information about the ring, centerX, centerY, radius
+directory = '/media/Argonne Backup/FFfine/' #directory of the files
+bgFile = directory + 'Ti7Test_00017.ge2' #filename of the background file
+filePrefix = 'Ti7_PreHRM_PreLoad__005' #prefix of each file
+ring = np.loadtxt(params,delimiter = ',') # loads the ring params into an array
+
+lowerID = 53 # ID of first binary
+upperID = 70 # ID of last binary
+
+"""
+No need to edit below this line
+"""
+
+IDs = np.linspace(lowerID, upperID, upperID - lowerID + 1)
+IDs = np.uint8(IDs) # creates an array of each ID to be iterated over in the loops
+
+a = np.arange(0,2048)
+yy = np.tile(a,[2048,1])
+a.shape = [2048,1]
+xx = np.tile(a, [1,2048])
 
 def makeMap(ringi):
-    
+    """
+    @param ringi int identifying the current ring to convert to eta phi map. 
+                Needed to obtain the center coordinates and radius of the ring 
+                to be converted.
+
+    This function makes an etaphi map out of the current ring. 
+    """
+
+    # coordinates of the center of the ring
     cenX = ring[ringi,0]
     cenY = ring[ringi,1]
     
+    # radius and eta arrays based on the center pixel
     radius = np.sqrt((xx - cenX)**2 + (yy-cenY)**2)
     ee = np.arctan2(cenY - yy, xx - cenX)*180/np.pi + 180
     
     r = ring[ringi,2]
     d = 10
 
+    # creates a boolean mask for the ring 
     ii = (radius > (r - d)) & (radius < (r + d))
     
-    eei = np.ceil(ee.T[ii.T]) # for compatability with MATLAB
+    # creates a vector of etas based on the mask
+    eei = np.ceil(ee.T[ii.T]) # the transposes are necessary for compatability with MATLAB
     eei[eei == 360] = 359
     
     etaInt = np.zeros((360,3600))
     tol = 500
     k = 0
+    
+    sys.stdout.write("Making Map: {0}".format(ringi))
+    sys.stdout.write("\n")
+    sys.stdout.write("[%s]"%(" "*40))
+    sys.stdout.write("\b" * 41)
+    
     for ID in IDs:
         for i in range(1,201):
             im = getFrame(directory,filePrefix, i, bgFile, ID, toler = tol)
@@ -36,38 +90,75 @@ def makeMap(ringi):
             for j in range(len(imc)):
                 etaInt[eei[j],k] = imc[j] + etaInt[eei[j],k]
             k = k + 1
+            if(i % 90 == 0):
+                sys.stdout.write("=")
+                sys.stdout.flush()
+                time.sleep(0.01)
+    sys.stdout.write("\n")
             #print k
     
     plotter(etaInt, ringi)
     
 def plotter(etaInt, ringi):
+    """
+    @param etaInt array containing the eta-phi map data for the ring
+    @param ringi int identifying the current ring
+    
+    This function plots the etaphi map as a scatter plot and saves it to the 
+    current directory.
+    """
+    
+    sys.stdout.write("Writing Images: {0}".format(ringi))
+    
     font = {'family': 'serif',
             'color': 'black',
             'weight': 'normal',
             'size': 14}
     
-    etaInt = np.flipud(etaInt)
+    ringNo = ringi + 1
+    
+    etaInt = np.flipud(etaInt) # the eta-phi array is flipped across the horizontal 
+                               # because the origin is at the top left prior to the flip 
+
+    etaInt = etaInt*(255.0/np.max(etaInt))
+        
+    plt.ion()
+    plt.figure()
+    plt.imshow(etaInt,origin='lower',
+               extent=[0, 180, 0, 360],aspect=0.5, cmap = plt.cm.YlOrBr)
+    plt.colorbar()
+    plt.grid()
+    
+    plt.xlabel(r'$\phi$', fontdict = font)
+    
+    plt.ylabel(r'$\eta$',rotation = 0, fontdict = font)
+    plt.title(r'$\eta$-$\phi$ Map, Ring '+str(ringNo), fontdict = font)
+
+    plt.savefig('eta-phi-map'+str(ringNo)+'.png')
     
     e = []
     p = []
     w = []
     
-    ringNo = ringi + 1
-    
     plt.ion()
     plt.figure()
     plt.axis([0,180,0,360])
     for i in range(3600):
-        ei = np.argwhere(etaInt[:,i])
-        pi = i/20.0
-        pi = np.array([pi]*len(ei))
-        wi = etaInt[ei,i]
+        ei = np.argwhere(etaInt[:,i]) # grabs the nonzero etas for the current phi
+        pi = i/20.0 # converts the phi to be from 0 to 180 instead of from 0 to 3600
+        pi = np.array([pi]*len(ei)) # makes sure that each eta in ei has a phi
+        wi = etaInt[ei,i] # grabs each integrated eta for the current phi
         
+        # combines all the etas and integrated etas for each phi into a single list
+        # for the scatter plot
         for j in range(len(ei)):
             e.append(ei[j,0])
             p.append(pi[j])
             w.append(wi[j,0])
+            #print j
     
+    # makes a scatter plot with the phi as the x coordinates, eta as the y coordinates and
+    # integrated etas as the weights
     plt.scatter(p,e,c = w, s = 20, cmap = plt.cm.jet, edgecolors = 'None', alpha = 0.75)
     plt.colorbar()
     plt.grid()
@@ -79,13 +170,34 @@ def plotter(etaInt, ringi):
     
     #plt.show()
     
-    plt.savefig('eta-phi-map'+str(ringNo)+'.png')
+    plt.savefig('eta-phi-map-scatter'+str(ringNo)+'.png')
+
 
 def getFrame(directory, filePrefix, frameNo = 1, bgFile = '', ID = 0, toler = 60,
               size = (2048,2048), header = 8192, frameSize = 2*2048**2):
-                
-    frameSize = 2*size[0]**2
+    """
+    @param directory string identifying the directory of all the binary files
+    @param filePrefix string identifying the prefix in the file name of all the 
+                binary files, if no common prefix a method to access each file 
+                has to be created.
+    @param frameNo int identifying the number of the frame in one file desired;
+                ranges from 1-200
+    @param bfile string identifying the file name of the background file
+    @param ID int identifying the binary file currently being accessesed, suffix
+                of the filename of the binary files
+    @param toler int minimum tolerance on the intensity data
+    @param size tuple containing the dimensions of the output image
+    @param header int number of bytes in the header of each binary file
+    @param frameSize int number of bytes in each frame
+    @return image_data uint16 array containing the intensity data of one frame 
+            with the background subtracted off and intensity above the threshold 
 
+    This function returns the desired frame with the intensity data above the
+    threshold and the background data subtracted off.
+    """
+
+    frameSize = 2*size[0]**2
+    #converts the background file to a uint16 array
     if(len(bgFile) != 0):
         bg_file = open(bgFile, 'rb')
         bg_file.seek(header)
@@ -106,7 +218,7 @@ def getFrame(directory, filePrefix, frameNo = 1, bgFile = '', ID = 0, toler = 60
         IDstr = str(ID)
         
     filename = directory + filePrefix + IDstr
-    
+    # reads the frame from the binary file into a string to be converted
     f = open(filename,'rb')
     offset = header + (frameNo - 1)*frameSize
     f.seek(offset)
@@ -117,23 +229,35 @@ def getFrame(directory, filePrefix, frameNo = 1, bgFile = '', ID = 0, toler = 60
     im = stats.threshold(im,threshmin = toler, newval = 0)
     return im
 
-params = 'circles.out'
-directory = '/media/Argonne Backup/FFfine/'
-bgFile = directory + 'Ti7Test_00017.ge2'
-filePrefix = 'Ti7_PreHRM_PreLoad__005'
-ring = np.loadtxt(params,delimiter = ',')
 
-lowerID = 53 # ID of first binary
-upperID = 70 # ID of last binary
-
-IDs = np.linspace(lowerID, upperID, upperID - lowerID + 1)
-IDs = np.uint8(IDs)
+def convertBin(im_data_hex, bg, size = (2048,2048)):
+    """
+    @param im_data_hex binary string containing the intensity data
+    @param bg uint16 array containing the intensity data of the background file
+    @param size tuple containing the dimensions of the output image
     
-a = np.arange(0,2048)
-yy = np.tile(a,[2048,1]) #y's
-a.shape = [2048,1] 
-xx = np.tile(a,[1,2048]) #x's
+    @return image_data uint16 array containing the intensity data of one frame 
+            with the background subtracted off.
 
-numRings = 11
-for i in range(numRings):
-    makeMap(i)
+    This function converts the intensity data in the binary string to a uint16 
+    array and subtracts of the background data.
+    """
+
+    image_data = np.fromstring(im_data_hex, dtype='uint16')
+    image_data.shape = size;
+    image_data = np.double(image_data)
+    image_data = image_data - bg
+
+    image_data = np.clip(image_data,0, 2**16 - 1)
+    image_data = np.uint16(image_data)
+    return image_data
+
+def main():
+    i = int(sys.argv[1])
+    if i < 10 and i > 0:
+        makeMap(i) # makes etaphi maps of each ring
+    else:
+        return
+    
+if __name__ == "__main__":
+    main()
--- radii.py
+++ radii.py
@@ -1,33 +1,35 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Nov  1 20:28:24 2013
+@File: radii.py
+@Author: Rohan Khanna
+@Date:Fri Nov 1, 2013
 
-@author: rkhanna2
+The following script determines the true center coordinates and true radius of
+each ring and writes it to a data file to be used by etaphi.py.
+Guesses of the radii of each ring need to be determined.
+
 """
+
+
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy import stats
-from readGE import convertBin
-from writeGE import writeGE
-
-filename = 'ring'
-offset = 8192
-size = (2048,2048)
-
-f = open(filename,'rb')
-f.seek(offset)
-image_data = f.read()
-image_data = np.double(convertBin(image_data,np.zeros(size)))
-f.close()
-
 
 def getCircles(guess,ringi):
+    """
+    @param guess The array of guesses for each ring
+    @param ringi int identifying the ring of which to determine the parameters
+
+    @return x the true center coordinates and true radius of the ring
+            ringi the intensity data of the pixels inside the bounds on the ring
+
+    This function determines the true center coordinates of the ring and the true radius
+    and also returns the ring data inside the bounds on the ring.
+    """
     cx = guess[0]
     cy = guess[1]
     r1 = guess[2]
     R0 = guess[3]
     r3 = guess[4]
-    
+    #vector for the intial guess for gauss newton method
     x_0 = np.array([[cx],[cy],[R0]])
     
     ring1 = np.zeros((2048,2048))
@@ -36,6 +38,8 @@ def getCircles(guess,ringi):
     yi = []
     Ii = []
     
+    # iterates through each pixel to determine the coordinates and intensity of 
+    # eacb pixel inside the bounds on the ring
     for i in range(2048):
         for j in range (2048):
             curr = np.sqrt((cx-i)**2 + (cy-j)**2)
@@ -50,12 +54,19 @@ def getCircles(guess,ringi):
     return x.T,ringi
     
 def r(xk,xi,yi):
+    """
+    The function returns the function values used in Gauss Newton Method
+    """
     rk = np.zeros((len(xi),1))
     for i in range(len(xi)):
         rk[i,0] = np.sqrt((xk[0,0]-xi[i])**2+(xk[1,0]-yi[i])**2)-xk[2,0]
     return rk
 
 def J(xk,xi,yi):
+    """
+    This function returns the jacobian of the function used in Gauss Newton 
+    Method.
+    """
     Jk = np.zeros((len(xi),3))
     
     for i in range(len(xi)):
@@ -66,6 +77,11 @@ def J(xk,xi,yi):
     return Jk
     
 def gnewtonm(x_0,xi,yi):
+    """
+    This function performs the Gauss Newton Method, a nonlinear least squares
+    method, to determine the true center coordinates and the true radius
+    """
+
     x = x_0
     tol = 1.0e-8
     error = 1.0
@@ -80,43 +96,46 @@ def gnewtonm(x_0,xi,yi):
         x = xk
         k = k + 1
     return x
-    
-def iringPlotter(x,ringi):
-#    plt.ion()
-#    plt.figure()
-    #xi = x[0]
-    #yi = x[1]
-    #Ri = x[2]
-    #r1 = Ri - 15
-    #r3 = Ri + 15    
-    #t = np.linspace(0,2*np.pi,1000)
-    
-    #plt.plot(r1*np.cos(t) + xi,r1*np.sin(t) + yi,'r:')
-    #plt.plot(Ri*np.cos(t) + xi,Ri*np.sin(t) + yi,'b:')
-    #plt.plot(r3*np.cos(t) + xi,r3*np.sin(t) + yi,'r:')
-    #plt.plot(xi,yi,'bo')
-    
-    plt.imshow(np.minimum(ringi, 255 + 0*ringi))
-    plt.hot()
-    plt.hold(True)
-   
-def ringPlotter(x, r1,r3):
-    plt.ion()
-    plt.figure()
-    xi = x[0,0]
-    yi = x[1,0]
-    Ri = x[2,0]
-    t = np.linspace(0,2*np.pi,1000)
-    plt.plot(r1*np.cos(t) + xi,r1*np.sin(t) + yi,'r--')
-    plt.plot(Ri*np.cos(t) + xi,Ri*np.sin(t) + yi,'b--')
-    plt.plot(r3*np.cos(t) + xi,r3*np.sin(t) + yi,'r--')
-    plt.imshow(np.minimum(stats.threshold(image_data,threshmin=60, newval=0), 255 + 0*image_data))
-    plt.hot()
-    
 
+
+def convertBin(im_data_hex, bg, size = (2048,2048)):
+    """
+    @param im_data_hex binary string containing the intensity data
+    @param bg uint16 array containing the intensity data of the background file
+    @param size tuple containing the dimensions of the output image
+    
+    @return image_data uint16 array containing the intensity data of one frame 
+            with the background subtracted off.
+
+    This function converts the intensity data in the binary string to a uint16 
+    array and subtracts of the background data.
+    """
+
+    image_data = np.fromstring(im_data_hex, dtype='uint16')
+    image_data.shape = size;
+    image_data = np.double(image_data)
+    image_data = image_data - bg
+
+    image_data = np.clip(image_data,0, 2**16 - 1)
+    image_data = np.uint16(image_data)
+    return image_data
+
+# this file contains all the intensity data in one binary file
+filename = 'ring'
+offset = 8192
+size = (2048,2048)
+
+f = open(filename,'rb')
+f.seek(offset)
+image_data = f.read()
+image_data = np.double(convertBin(image_data,np.zeros(size)))
+f.close()
+
+# guesses is an array of estimates of the parameters of each ring
 guesses = np.zeros((11,5))
-guesses[:,0:2] = [1024.0,1024.0]
-guesses[0,2:5] = [410.0,420.0,440.0]
+guesses[:,0:2] = [1024.0,1024.0] # center coordinate of each ring
+# lower bound on estimate on radius, guess of radius, upper bound on estimate of radius
+guesses[0,2:5] = [410.0,420.0,440.0] 
 guesses[1,2:5] = [445.0,460.0,470.0]
 guesses[2,2:5] = [470.0,485.0,500.0]
 guesses[3,2:5] = [615.0,635.0,650.0]
@@ -128,16 +147,11 @@ guesses[8,2:5] = [883.0,897.0,901.0]
 guesses[9,2:5] = [910.0,925.0,945.0]
 guesses[10,2:5] = [955.0,975.0,995.0]
 
-circles = np.zeros((11,3))
+# circles is the parameter array containing the true center and true radius
+circles = np.zeros((11,3)) 
 ringi = np.zeros((2048,2048))
 for i in range(11):
     circles[i,:],ringi = getCircles(guesses[i,:],ringi)
-    iringPlotter(circles[i,:],ringi)
 
-print circles
-ringi = np.uint16(ringi)
-ringi.shape = (2048,2048)
-
-writeGE(ringi, '/home/tempuser/Rohan/', 'ring', 'rings1-11', 0)
-
+#writes the ring parameters to a file to be read by etaphi.py
 np.savetxt('circles.out', circles, delimiter=',') 
